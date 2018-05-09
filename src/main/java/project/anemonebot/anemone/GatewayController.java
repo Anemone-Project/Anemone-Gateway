d77c91acc6f92e1be840821808456b2916e88ba3
Seophyn <Jimmygustafsson93@gmail.com>
2018-05-09 14:30:28 +0200

    Adds endpoint for connection between micro-services

---
+++ src/main/java/project/anemonebot/anemone/GatewayController.java
@@ -0,0 +1,50 @@
+package project.anemonebot.anemone;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.*;
+
+
+/**
+ * Controller-class for incoming HTTP-calls to Anemone endpoints.
+ */
+
+@RestController()
+@RequestMapping(value = "/anemone")
+public class GatewayController {
+
+    Logger logger = LogManager.getLogger(getClass());
+
+    /**
+     * Endpoint for testing connection to micro-service.
+     * @return new Greeting object.
+     */
+    @RequestMapping(value = "/anemonetest", method = RequestMethod.GET, produces = "text/plain")
+    public String anemoneConnectionTest() {
+
+        return "Connection OK!";
+    }
+
+
+    /**
+     * Endpoint for Github Webhook
+     * @param jsonString
+     */
+    @RequestMapping(value = "/github", method = RequestMethod.POST)
+    public void receiveGithubJSON(@RequestBody String jsonString){
+        logger.debug(jsonString);
+    }
+
+
+    /**
+     * Endpoint for testing connection between micro-services.
+     * @param word Used to gauge whether or not request reached end point.
+     * @return
+     */
+    @RequestMapping(value = "/anemoneintergrationtest/{word}", method = RequestMethod.GET)
+    public String anemoneIntergrationTest(@PathVariable ("word") String word){
+        return word + " Gateway";
+    }
+
+
+}
